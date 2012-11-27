404cc16aaf3aad8a47cd89d240dd677016cf1cd5
Bruno Thomas <bruno@barreverte.fr>
2012-11-28 00:02:39 +0100

    init

---
+++ src/main/current_cost.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+from datetime import time, datetime
+from xml.etree.ElementTree import XML, XMLParser
+import serial
+import MySQLdb as mdb
+
+__author__ = 'bruno'
+
+ser = serial.Serial('/dev/ttyUSB0', baudrate=57600,
+                    bytesize=serial.EIGHTBITS,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    timeout=10)
+con = mdb.connect('192.168.0.10', 'test', '', 'test')
+try:
+    with con:
+        cur = con.cursor()
+        while True:
+            line = ser.readline()
+            if line:
+                xml_data = XML(line, XMLParser())
+                if len(xml_data) >= 7 and xml_data[2].tag == 'time' and xml_data[7].tag == 'ch1':
+                    time_array = xml_data[2].text.split(':')
+                    timestamp = datetime.combine(datetime.today(), time(int(time_array[0]), int(time_array[1]), int(time_array[2])))
+                    power = int(xml_data[7][0].text)
+                    print '%s : %s (%s°C)' % (timestamp, power, xml_data[3].text)
+                    cur.execute('insert into current_cost (timestamp, watt, temperature) values (%s, %s, %s)', (timestamp, power, xml_data[3].text))
+
+finally:
+    print 'closing'
+    ser.close()
+    con.close()
