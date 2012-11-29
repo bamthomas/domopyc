7079f78ba6addc337c530370d807ab10d047307c
Bruno Thomas <bruno@barreverte.fr>
2012-11-30 00:19:01 +0100

    RedisSubscriber.wait_for_messages

--- src/main/current_cost.py
+++ src/main/current_cost.py
@@ -1,24 +1,57 @@
 # coding=utf-8
 from datetime import datetime
+from json import loads
+from threading import Thread, Event
 from xml.etree.ElementTree import XML, XMLParser
 import serial
+import redis
 
+CURRENT_COST = 'current_cost'
+REDIS = redis.Redis()
 __author__ = 'bruno'
 
-ser = serial.Serial('/dev/ttyUSB0', baudrate=57600,
-                    bytesize=serial.EIGHTBITS,
-                    parity=serial.PARITY_NONE,
-                    stopbits=serial.STOPBITS_ONE,
-                    timeout=10)
-try:
-    while True:
-        line = ser.readline()
-        if line:
-            xml_data = XML(line, XMLParser())
-            if len(xml_data) >= 7 and xml_data[2].tag == 'time' and xml_data[7].tag == 'ch1':
-                power = int(xml_data[7][0].text)
-                print '%s : %s (%s°C)' % (datetime.now(), power, xml_data[3].text)
 
-finally:
-    print 'closing'
-    ser.close()
+class CurrentCostReader(Thread):
+    def __init__(self, myredis):
+        super(CurrentCostReader, self).__init__(target=self.read_sensor)
+        self.serial = serial.Serial('/dev/ttyUSB0', baudrate=57600,
+            bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=10)
+        self.redis = myredis
+        self.stop = Event()
+
+    def read_sensor(self):
+        try:
+            while not self.stop.is_set():
+                line = self.serial.readline()
+                if line:
+                    xml_data = XML(line, XMLParser())
+                    if len(xml_data) >= 7 and xml_data[2].tag == 'time' and xml_data[7].tag == 'ch1':
+                        power = int(xml_data[7][0].text)
+                        print '%s : %s (%s°C)' % (datetime.now(), power, xml_data[3].text)
+                        self.redis.publish(CURRENT_COST,{'timestamp':datetime.now().isoformat(), 'watt':power, 'temperature':xml_data[3].text})
+        finally:
+            print 'closing'
+            self.serial.close()
+
+    def stop(self):
+        self.stop.set()
+        
+class RedisSubscriber(Thread):
+    def __init__(self, redis, callback_func):
+        super(RedisSubscriber, self).__init__(target=self.wait_messages)
+        self.callback_func = callback_func
+        self.redis = redis
+        self.pubsub = redis.pubsub()
+        self.pubsub.subscribe([CURRENT_COST])
+
+    def wait_messages(self):
+        for item in self.pubsub.listen():
+            if item['type'] == 'message':
+                self.callback_func(loads(item['data']))
+
+    def stop(self):
+        self.pubsub.unsubscribe()
+
+if __name__ == '__main__':
+    current_cost = CurrentCostReader(REDIS)
+    current_cost.start()
---
+++ src/test/test_current_cost.py
@@ -0,0 +1,33 @@
+from Queue import Queue
+from json import dumps
+import unittest
+from current_cost import CURRENT_COST, RedisSubscriber
+from datetime import datetime
+import redis
+
+__author__ = 'bruno'
+
+
+class RedisSubscriberTest(unittest.TestCase):
+    def setUp(self):
+        self.queue = Queue()
+        def test_callback(event):
+            self.queue.put(event)
+        self.myredis = redis.Redis()
+        self.subscriber = RedisSubscriber(self.myredis, test_callback)
+        self.subscriber.start()
+
+    def tearDown(self):
+        self.subscriber.stop()
+        self.subscriber.join()
+
+    def test_reader(self):
+        expected = {'date': datetime.now().isoformat(), 'watt': '123', 'temperature': '23.4'}
+        self.myredis.publish(CURRENT_COST, dumps(expected))
+        event = self.queue.get()
+        self.assertIsNotNone(event)
+        self.assertDictEqual(event, expected)
+
+
+
+
