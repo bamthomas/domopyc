74b9a9caa2be513097e2fae1d2a40bbf9e5904dc
Bruno Thomas <bruno@barreverte.fr>
2012-12-14 00:36:36 +0100

    script sh sans erreurs de syntaxe

--- batches/import_csv_mysql.sh
+++ batches/import_csv_mysql.sh
@@ -1,4 +1,6 @@
-CREATE TABLE `current_cost` (
+#!/bin/sh
+
+CREATE_TABLE_SQL='CREATE TABLE `current_cost` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `watt` int(11) DEFAULT NULL,
@@ -6,6 +8,7 @@ CREATE TABLE `current_cost` (
   `nb_data` int(11) DEFAULT NULL,
   `temperature` float DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8
+) ENGINE=MyISAM DEFAULT CHARSET=utf8'
 
-LOAD data local infile '/share/current_cost/current_cost_2012-12-12.csv' into table test FIELDS TERMINATED BY ';' LINES TERMINATED BY '\n' ignore 1 lines (timestamp, watt, minutes, nb_data, temperature);
+LOAD_DATA_SQL="LOAD data local infile '/share/current_cost/current_cost_2012-12-12.csv' into table current_cost FIELDS TERMINATED BY ';' LINES TERMINATED BY '\n' ignore 1 lines (timestamp, watt, minutes, nb_data, temperature);"
+
