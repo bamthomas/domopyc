5b96f58aa6e69c5e4fb5cc449b07819fde366e4b
Bruno Thomas <bruno@barreverte.fr>
2015-05-08 01:29:30 +0200

    [add] porting threaded redis subscriber to asyncio

---
+++ src/current_cost/sensor/tests/test_current_cost_async.py
@@ -0,0 +1,102 @@
+from asyncio import Queue
+import asyncio
+from json import dumps, loads
+import unittest
+from datetime import datetime, timezone
+
+import asyncio_redis
+import functools
+import iso8601
+
+__author__ = 'bruno'
+
+
+
+@asyncio.coroutine
+def create_redis_pool():
+    connection = yield from asyncio_redis.Pool.create(host='localhost', port=6379, poolsize=4)
+    return connection
+
+
+def async_coro(f):
+    def wrap(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            coro = asyncio.coroutine(f)
+            future = coro(*args, **kwargs)
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(future)
+        return wrapper
+    return wrap(f)
+
+
+class WithRedis(unittest.TestCase):
+    @asyncio.coroutine
+    def setUp(self):
+        self.connection = yield from create_redis_pool()
+        yield from self.connection.flushdb()
+
+    @asyncio.coroutine
+    def tearDown(self):
+        self.connection.close()
+
+
+class AsyncRedisSubscriber(object):
+    infinite_loop = lambda i: True
+    wait_value = lambda n: lambda i: i < n
+
+    def __init__(self, redis_conn, message_handler, pubsub_key):
+        self.pubsub_key = pubsub_key
+        self.message_handler = message_handler
+        self.redis_conn = redis_conn
+        self.subscriber = None
+        self.message_loop_task = None
+        asyncio.new_event_loop().run_until_complete(self.setup_subscriber())
+
+    @asyncio.coroutine
+    def setup_subscriber(self):
+        self.subscriber = yield from self.redis_conn.start_subscribe()
+        yield from self.subscriber.subscribe([self.pubsub_key])
+
+    def start(self, for_n_messages=0):
+        predicate = AsyncRedisSubscriber.infinite_loop if for_n_messages == 0 else AsyncRedisSubscriber.wait_value(for_n_messages)
+        self.message_loop_task = asyncio.async(self.message_loop(predicate))
+        return self
+
+    @asyncio.coroutine
+    def message_loop(self, predicate):
+        i = 0
+        print("pouet")
+        while predicate(i):
+            i += 1
+            print('avant')
+            message_str = yield from self.subscriber.next_published()
+            print(message_str.value)
+            message = loads(message_str.value)
+            message['date'] = iso8601.parse_date(message['date'])
+            yield from self.message_handler.handle(message)
+
+
+class RedisSubscribeLoopTest(WithRedis):
+    @async_coro
+    def setUp(self):
+        yield from super().setUp()
+        class TestMessageHandler(object):
+            queue = Queue()
+            @asyncio.coroutine
+            def handle(self, message):
+                yield from self.queue.put(message)
+        self.message_handler = TestMessageHandler()
+        self.subscriber = AsyncRedisSubscriber(self.connection, self.message_handler, 'key')
+
+    @async_coro
+    def test_subscribe_loop(self):
+        self.subscriber.start(1)
+        now = datetime.now(timezone.utc)
+        expected = {'watt': '123'}
+
+        yield from self.connection.publish('key', dumps(dict(expected, date=now.isoformat())))
+
+        event = yield from asyncio.wait_for(self.message_handler.queue.get(), 2)
+        self.assertIsNotNone(event)
+        self.assertDictEqual(event, dict(expected, date=now))
