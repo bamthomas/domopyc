306e290a615587327e6ced3fda949c4f048dc30e
Bruno Thomas <bruno@barreverte.fr>
2015-05-18 22:15:09 +0200

    [add] added redis pool creation for readers

--- src/daq/publishers/redis_publisher.py
+++ src/daq/publishers/redis_publisher.py
@@ -3,6 +3,16 @@ import asyncio
 from json import dumps
 
 
+import asyncio_redis
+
+
+#duplicate from redis_toolbox
+@asyncio.coroutine
+def create_redis_pool(size):
+    connection = yield from asyncio_redis.Pool.create(host='localhost', port=6379, poolsize=size)
+    return connection
+
+
 class RedisPublisher(object):
 
     def __init__(self, redis_conn, key):
