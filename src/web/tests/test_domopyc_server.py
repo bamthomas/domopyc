7574515181fb8ef8308510653c2e3451298c127d
Bruno Thomas <bruno@barreverte.fr>
2015-05-29 21:37:26 +0200

    [ut][fix] green bar !

--- src/web/tests/test_domopyc_server.py
+++ src/web/tests/test_domopyc_server.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from json import dumps
 import asyncio
+import os
 
 from iso8601_json import Iso8601DateEncoder
 from test_utils.ut_async import async_coro
@@ -16,6 +17,7 @@ class RedisGetDataOfDay(WithRedis):
     @async_coro
     def setUp(self):
         yield from super().setUp()
+        os.chdir(os.path.dirname(os.path.realpath(__file__)) + '/..')
         yield from domopyc_server.init(asyncio.get_event_loop())
         yield from self.connection.delete([self.redis_key])
 
@@ -25,12 +27,12 @@ class RedisGetDataOfDay(WithRedis):
         expected_json = {'date': domopyc_server.now(), 'watt': 305, 'temperature': 21.4}
         yield from self.connection.lpush(self.redis_key, [dumps(expected_json, cls=Iso8601DateEncoder)])
 
-        data = yield from get_current_cost_data()
+        data = yield from get_current_cost_data(self.connection)
         self.assertEquals(len(data), 1)
         self.assertEquals(data, [expected_json])
 
         yield from self.connection.lpush(self.redis_key,[dumps({'date': datetime.now() + timedelta(seconds=7), 'watt': 432, 'temperature': 20}, cls=Iso8601DateEncoder)])
-        self.assertEquals(len((yield from get_current_cost_data())), 2)
+        self.assertEquals(len((yield from get_current_cost_data(self.connection))), 2)
 
     @property
     def redis_key(self):
