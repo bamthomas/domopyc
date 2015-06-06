9c5d27c504f2be0865a1d5cfcfe987a5cc53ec46
Bruno Thomas <bruno@barreverte.fr>
2015-06-07 00:14:27 +0200

    [web] mysql request for costs with empty and full hours

--- src/web/current_cost_mysql_service.py
+++ src/web/current_cost_mysql_service.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 import asyncio
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
+from decimal import Decimal
 
 
 class CurrentCostDatabaseReader(object):
-    def __init__(self, pool):
+    def __init__(self, pool, full_hours_start=time(0, 0), full_hours_stop=time(23, 59, 59)):
+        self.full_hours_stop = full_hours_stop
+        self.full_hours_start = full_hours_start
         self.pool = pool
 
     @asyncio.coroutine
@@ -19,18 +22,6 @@ class CurrentCostDatabaseReader(object):
             return result
 
     @asyncio.coroutine
-    def get_costs(self, since):
-        with (yield from self.pool) as conn:
-            cur = yield from conn.cursor()
-            yield from cur.execute("SELECT timestamp(date(timestamp), MAKETIME(0,0,0)) AS day, "
-                                   "sum(watt * minutes) / (60 * 1000) FROM current_cost "
-                                   "where UNIX_TIMESTAMP(timestamp) > %s "
-                                   "GROUP BY date(timestamp) ORDER BY day " % since)
-            result = yield from cur.fetchall()
-            yield from cur.close()
-            return result
-
-    @asyncio.coroutine
     def get_by_day(self, date_time):
         with (yield from self.pool) as conn:
             cur = yield from conn.cursor()
@@ -39,3 +30,33 @@ class CurrentCostDatabaseReader(object):
             result = yield from cur.fetchall()
             yield from cur.close()
             return result
+
+    @asyncio.coroutine
+    def get_costs(self, since):
+        with (yield from self.pool) as conn:
+            cur = yield from conn.cursor()
+            yield from cur.execute("SELECT timestamp(date(timestamp), MAKETIME(0,0,0)) AS day, "
+                                   "sum(watt * minutes) / (60 * 1000) FROM current_cost "
+                                   "WHERE timestamp >= %s AND TIME(timestamp) >= %s and TIME(timestamp) <= %s "
+                                   "GROUP BY date(timestamp) ORDER BY day ", (since, self.full_hours_start, self.full_hours_stop ))
+            full = yield from cur.fetchall()
+            yield from cur.execute("SELECT timestamp(date(timestamp), MAKETIME(0,0,0)) AS day, "
+                                   "sum(watt * minutes) / (60 * 1000) FROM current_cost "
+                                   "WHERE timestamp >= %s AND (TIME(timestamp) < %s OR TIME(timestamp) > %s) "
+                                   "GROUP BY date(timestamp) ORDER BY day ", (since, self.full_hours_start, self.full_hours_stop))
+            empty = yield from cur.fetchall()
+            yield from cur.close()
+            return merge_full_and_empty_hours(full, empty)
+
+
+def merge_full_and_empty_hours(full, empty):
+    print(full)
+    print(empty)
+
+    full_hours_dict = dict(full)
+    empty_hours_dict = dict(empty)
+
+    merged_dict_from_full = {k: (v, empty_hours_dict.get(k, Decimal(0))) for (k, v) in full_hours_dict.items()}
+    merged_dict_from_empty = {k: (full_hours_dict.get(k, Decimal(0)), v) for (k, v) in empty_hours_dict.items()}
+    merged_dict_from_full.update(merged_dict_from_empty)
+    return tuple(sorted(merged_dict_from_full.items()))
--- src/web/tests/test_current_cost_mysql_service.py
+++ src/web/tests/test_current_cost_mysql_service.py
@@ -1,12 +1,12 @@
 import asyncio
-from datetime import datetime, timezone
+from datetime import datetime, timezone, time
 from decimal import Decimal
 from unittest import TestCase
 
 import aiomysql
 from subscribers.mysql_toolbox import MysqlAverageMessageHandler
 from test_utils.ut_async import async_coro
-from web.current_cost_mysql_service import CurrentCostDatabaseReader
+from web.current_cost_mysql_service import CurrentCostDatabaseReader, merge_full_and_empty_hours
 
 
 class GetCurrentCostData(TestCase):
@@ -17,7 +17,7 @@ class GetCurrentCostData(TestCase):
                                                     loop=asyncio.get_event_loop())
 
         self.message_handler = MysqlAverageMessageHandler(self.pool)
-        self.current_cost_service = CurrentCostDatabaseReader(self.pool)
+        self.current_cost_service = CurrentCostDatabaseReader(self.pool, time(8, 0), time(22, 0))
         with (yield from self.pool) as conn:
             cur = yield from conn.cursor()
             yield from cur.execute("truncate current_cost")
@@ -29,10 +29,10 @@ class GetCurrentCostData(TestCase):
 
     @async_coro
     def test_get_history_three_days(self):
-        yield from self.message_handler.save({'date': datetime(2015, 5, 28, 12, 0, 0, tzinfo=timezone.utc), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
-        yield from self.message_handler.save({'date': datetime(2015, 5, 29, 10, 10, 0, tzinfo=timezone.utc), 'watt': 1000, 'minutes': 120, 'nb_data': 120, 'temperature': 20.2})
-        yield from self.message_handler.save({'date': datetime(2015, 5, 29, 12, 10, 0, tzinfo=timezone.utc), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
-        yield from self.message_handler.save({'date': datetime(2015, 5, 30, 12, 20, 0, tzinfo=timezone.utc), 'watt': 1000, 'minutes': 180, 'nb_data': 120, 'temperature': 20.2})
+        yield from self.message_handler.save({'date': datetime(2015, 5, 28, 12, 0, 0), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
+        yield from self.message_handler.save({'date': datetime(2015, 5, 29, 10, 10, 0), 'watt': 1000, 'minutes': 120, 'nb_data': 120, 'temperature': 20.2})
+        yield from self.message_handler.save({'date': datetime(2015, 5, 29, 12, 10, 0), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
+        yield from self.message_handler.save({'date': datetime(2015, 5, 30, 12, 20, 0), 'watt': 1000, 'minutes': 180, 'nb_data': 120, 'temperature': 20.2})
 
         data = yield from self.current_cost_service.get_history()
 
@@ -42,10 +42,44 @@ class GetCurrentCostData(TestCase):
         self.assertEqual((datetime(2015, 5, 30, 0, 0), Decimal(3)), data[2])
 
     @async_coro
-    def test_get_costs(self):
-        yield from self.message_handler.save({'date': datetime(2015, 5, 28, 12, 0, 0, tzinfo=timezone.utc), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
-        yield from self.message_handler.save({'date': datetime(2015, 5, 29, 10, 10, 0, tzinfo=timezone.utc), 'watt': 1000, 'minutes': 120, 'nb_data': 120, 'temperature': 20.2})
+    def test_get_costs_since_and_only_empty_hours(self):
+        yield from self.message_handler.save({'date': datetime(2015, 5, 28, 12, 0, 0), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
+        yield from self.message_handler.save({'date': datetime(2015, 5, 30, 7, 0, 0), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
+
+        data = yield from self.current_cost_service.get_costs(since=datetime(2015, 5, 30))
+
+        self.assertTupleEqual((datetime(2015, 5, 30), (Decimal(0.0), Decimal(1.0))), data[0])
+
+    @async_coro
+    def test_get_costs_without_empty_and_full_hours(self):
+        current_cost_service_without_discount_hours = CurrentCostDatabaseReader(self.pool)
+        yield from self.message_handler.save({'date': datetime(2015, 5, 28, 12, 0, 0), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
+        yield from self.message_handler.save({'date': datetime(2015, 5, 28, 7, 0, 0), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
+
+        data = yield from current_cost_service_without_discount_hours.get_costs(since=datetime(2015, 5, 28, 0, 0))
+
+        self.assertEqual(1, len(data))
+        self.assertEqual((datetime(2015, 5, 28), (Decimal(2.0), Decimal(0.0))), data[0])
+
+    @async_coro
+    def test_get_costs_full_and_empty_hours(self):
+        yield from self.message_handler.save({'date': datetime(2015, 5, 28, 12, 0, 0), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
+        yield from self.message_handler.save({'date': datetime(2015, 5, 28, 7, 0, 0), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
+        yield from self.message_handler.save({'date': datetime(2015, 5, 28, 23, 0, 0), 'watt': 1000, 'minutes': 60, 'nb_data': 120, 'temperature': 20.2})
 
         data = yield from self.current_cost_service.get_costs(since=datetime(2015, 5, 28, 0, 0).timestamp())
 
-        self.assertEqual(2, len(data))
+        self.assertEqual(1, len(data))
+        self.assertEqual((datetime(2015, 5, 28), (Decimal(1.0), Decimal(2.0))), data[0])
+
+
+class MergeEmptyAndFullHours(TestCase):
+    def test_no_empty_hours(self):
+        self.assertTupleEqual(((datetime(2015, 5, 28), (1, 0)),), merge_full_and_empty_hours(((datetime(2015, 5, 28), 1),), ()))
+
+    def test_with_empty_hours(self):
+        self.assertTupleEqual(((datetime(2015, 5, 28), (1, 2)),), merge_full_and_empty_hours(((datetime(2015, 5, 28), 1), ), ((datetime(2015, 5, 28), 2), )))
+
+    def test_with_empty_hours_on_different_date(self):
+        self.assertTupleEqual(((datetime(2015, 5, 28), (1, 0)), (datetime(2015, 5, 29), (0, 2))),
+                              merge_full_and_empty_hours(((datetime(2015, 5, 28), 1),), ((datetime(2015, 5, 29), 2),)))
