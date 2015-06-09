f960b2d9b60f9601aa2ab3505172afc19678e0a7
Bruno Thomas <bruno@barreverte.fr>
2015-06-09 23:16:25 +0200

    [web] websocket test for live data

--- src/web/tests/test_domopyc_server_live.py
+++ src/web/tests/test_domopyc_server_live.py
@@ -1,11 +1,8 @@
-from datetime import datetime, timezone
-from json import loads, dumps
 import asyncio
 
 import aiohttp
-from iso8601_json import Iso8601DateEncoder
+from daq.current_cost_sensor import CURRENT_COST_KEY
 import os
-from subscribers import redis_toolbox
 from test_utils.ut_async import async_coro
 from test_utils.ut_redis import WithRedis
 from web import domopyc_server
@@ -19,7 +16,6 @@ class GetLiveData(WithRedis):
         yield from super().setUp()
         os.chdir(os.path.dirname(os.path.realpath(__file__)) + '/..')
         self.server = yield from domopyc_server.init(asyncio.get_event_loop())
-        yield from self.connection.delete([self.redis_key])
 
     @async_coro
     def tearDown(self):
@@ -27,14 +23,9 @@ class GetLiveData(WithRedis):
 
     @async_coro
     def test_live_data(self):
-        now = datetime.now(tz=timezone.utc)
-        yield from self.connection.zadd('current_cost_live_data', {dumps({'date': now, 'watt': 305}, cls=Iso8601DateEncoder): redis_toolbox.now().timestamp()})
+        ws = yield from aiohttp.ws_connect('http://localhost:8080/livedata/power')
+        yield from self.connection.publish(CURRENT_COST_KEY, 'a message')
+        message = yield from asyncio.wait_for(ws.receive(), 2)
 
-        response = yield from aiohttp.request('GET', 'http://127.0.0.1:8080/data_since/5')
-
-        val = yield from response.read()
-        self.assertEqual(loads(val.decode()), {'points': [{'date': now.isoformat(), 'watt': 305}]})
-
-    @property
-    def redis_key(self):
-        return 'current_cost_%s' % datetime.now().strftime('%Y-%m-%d')
+        self.assertEqual('a message', message.data)
+        yield from ws.close()
