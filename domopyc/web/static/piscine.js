c7f4fd84e2d9ea00d58a7c5943b850376495360c
Bruno Thomas <bruno@barreverte.fr>
2015-08-28 23:39:54 +0200

    [web] area gradient for pool temp

--- domopyc/web/static/piscine.js
+++ domopyc/web/static/piscine.js
@@ -24,11 +24,33 @@ $(document).ready(function () {
                 text: '°C'
             }
         },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {x1: 0, y1: 0, x2: 0, y2: 1},
+                    stops: [
+                        [0,'#DF7514'],
+                        [1, 'white']
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
         legend: {
             enabled: false
         },
         series: [
             {
+                type: 'area',
                 name: 'Température piscine',
                 data: POOL_TEMPERATURES,
                 color: '#DF7514'
